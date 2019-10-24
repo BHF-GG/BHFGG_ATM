fd428d792f7b7bf93f71fe70f140755b467bb331
Henrik T. Jensen <HenrikThueJensen@gmail.com>
2019-10-24 13:42:37 +0200

    Added: NUnit package

--- BHFGG_ATM/Interfaces/IConditionChecker.cs
+++ BHFGG_ATM/Interfaces/IConditionChecker.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using BHFGG_ATM.Classes;
+using BHFGG_ATM.EventArgClasses;
 
 namespace BHFGG_ATM.Interfaces
 {
     public interface IConditionChecker
     {
         void CheckCondition(List<Track> tracks);
+        event EventHandler<ConditionCheckedEventArgs> ConditionsCheckedEvent;
     }
 }
