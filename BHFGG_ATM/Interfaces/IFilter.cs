09321c6ba8b01cd2bbb7318baf954698808a6268
Bertram <bw-sp@live.dk>
2019-10-24 12:53:41 +0200

    Sync for public class

--- BHFGG_ATM/Interfaces/IFilter.cs
+++ BHFGG_ATM/Interfaces/IFilter.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BHFGG_ATM.Classes;
+using BHFGG_ATM.EventArgClasses;
 
 namespace BHFGG_ATM.Interfaces
 {
     interface IFilter
     {
+        void FilterData(List<Track> trackList);
+        event EventHandler<DataFilteredEventArgs> DataFilteredEvent;
     }
 }
