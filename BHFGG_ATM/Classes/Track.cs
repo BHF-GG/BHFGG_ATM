ece4acb0e3fa6a34e4fe0ea4e0cb646d561627fd
FrandsAndersen <frands1995@gmail.com>
2019-10-24 12:58:31 +0200

    Added fields from classdiagram to Track class

--- BHFGG_ATM/Classes/Track.cs
+++ BHFGG_ATM/Classes/Track.cs
@@ -8,5 +8,21 @@ namespace BHFGG_ATM.Classes
 {
     public class Track
     {
+        public double GetCurrentVelocity()
+        {
+            double velocity = 0;
+
+            //calculate velocity
+
+            return velocity;
+        }
+
+        public string Tag;
+        public double PositionY;
+        public double PositionX;
+        public double Altitude;
+        public double HorizontalVelocity;
+        public double CompassCourse;
+
     }
 }
