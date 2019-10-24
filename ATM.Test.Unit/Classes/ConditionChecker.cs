4812fd56f3b75030d93f8e1235e28d9ba338fd22
Bertram <bw-sp@live.dk>
2019-10-24 12:27:17 +0200

    Added empty classes and interfaces, classes inherrits from interfaces

---
+++ ATM.Test.Unit/Classes/ConditionChecker.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ATM.Test.Unit.Interfaces;
+
+namespace ATM.Test.Unit.Classes
+{
+    class ConditionChecker : IConditionChecker
+    {
+    }
+}
