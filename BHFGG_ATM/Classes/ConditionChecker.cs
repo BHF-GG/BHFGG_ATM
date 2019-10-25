c27516d44a07c24b89e0555f99f55959ff631e84
Henrik T. Jensen <HenrikThueJensen@gmail.com>
2019-10-25 10:39:00 +0200

    Edited: GetDistance - Removed if (result < 0)

--- BHFGG_ATM/Classes/ConditionChecker.cs
+++ BHFGG_ATM/Classes/ConditionChecker.cs
@@ -73,8 +73,6 @@ namespace BHFGG_ATM.Classes
 
             int result = Convert.ToInt32(Math.Sqrt(Math.Pow(deltaX, 2) + Math.Pow(deltaY, 2)));
 
-            if (result < 0)
-                result = result * (-1);
             return result;
         }
 
